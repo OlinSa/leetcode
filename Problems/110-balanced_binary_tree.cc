84fbe34152c0200c7b691472ac6b33b782db78bd
Olin <cuijianjoy@126.com>
2024-08-01 09:46:40 +0800

    [Problems] 110-balanced_binary_tree
    Signed-off-by: Olin <cuijianjoy@126.com>

---
+++ Problems/110-balanced_binary_tree.cc
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  bool isBalanced(TreeNode *root) {
+    if (root == nullptr) return true;
+    int left_depth = 0;
+    int right_depth = 0;
+    maxDepth(root->left, 1, left_depth);
+    maxDepth(root->right, 1, right_depth);
+    return std::abs(left_depth - right_depth) <= 1 && isBalanced(root->left) &&
+           isBalanced(root->right);
+  }
+  void maxDepth(TreeNode *node, int depth, int &max_depth) {
+    if (node == nullptr) {
+      return;
+    }
+    max_depth = std::max(max_depth, depth);
+    maxDepth(node->left, depth + 1, max_depth);
+    maxDepth(node->right, depth + 1, max_depth);
+  }
+};
+
+TreeNode *general_test_data_prorder(std::vector<int> list) {
+  if (list.empty() || list[0] == -1)  // Edge case: empty list or root is null
+    return nullptr;
+
+  TreeNode *root = new TreeNode(list[0]);
+  std::queue<TreeNode *> q;
+  q.push(root);
+  size_t index = 1;  // Start from the second element in the list
+
+  while (!q.empty() && index < list.size()) {
+    TreeNode *current = q.front();
+    q.pop();
+
+    // Create left child
+    if (list[index] != -1) {
+      current->left = new TreeNode(list[index]);
+      q.push(current->left);
+    }
+    index++;
+
+    // Create right child
+    if (index < list.size() && list[index] != -1) {
+      current->right = new TreeNode(list[index]);
+      q.push(current->right);
+    }
+    index++;
+  }
+
+  return root;
+}
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  std::cout << s.isBalanced(general_test_data_prorder(
+                   std::vector<int>{3, 9, 20, -1, -1, 15, 7}))
+            << std::endl;
+  std::cout << s.isBalanced(general_test_data_prorder(
+                   std::vector<int>{1, 2, 2, 3, 3, -1, -1, 4, 4}))
+            << std::endl;
+  std::cout << s.isBalanced(general_test_data_prorder(std::vector<int>{-1}))
+            << std::endl;
+  std::cout << s.isBalanced(general_test_data_prorder(
+                   std::vector<int>{1, 2, 2, 3, -1, -1, 3, 4, -1, -1, 4}))
+            << std::endl;
+  return 0;
+}
