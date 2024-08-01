9693d2d437c22ff01bca47bf59a5782a62a63a8d
Olin <cuijianjoy@126.com>
2024-08-01 10:50:01 +0800

    [Problems] 112-path_sum
    Signed-off-by: Olin <cuijianjoy@126.com>

---
+++ Problems/112-path_sum.cc
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  bool hasPathSum(TreeNode *root, int targetSum) {
+    return preOrder(root, targetSum);
+  }
+  bool preOrder(TreeNode *node, int sum) {
+    if (node == nullptr) return false;
+    if (node->val == sum && node->left == nullptr && node->right == nullptr)
+      return true;
+    return preOrder(node->left, sum - node->val) ||
+           preOrder(node->right, sum - node->val);
+  }
+};
+
+TreeNode *general_test_data_prorder(std::vector<int> list) {
+  if (list.empty() || list[0] == -1)  // Edge case: empty list or root is null
+    return nullptr;
+
+  TreeNode *root = new TreeNode(list[0]);
+  std::queue<TreeNode *> q;
+  q.push(root);
+  size_t index = 1;  // Start from the second element in the list
+
+  while (!q.empty() && index < list.size()) {
+    TreeNode *current = q.front();
+    q.pop();
+
+    // Create left child
+    if (list[index] != -1) {
+      current->left = new TreeNode(list[index]);
+      q.push(current->left);
+    }
+    index++;
+
+    // Create right child
+    if (index < list.size() && list[index] != -1) {
+      current->right = new TreeNode(list[index]);
+      q.push(current->right);
+    }
+    index++;
+  }
+
+  return root;
+}
+
+int main(int argc, char *argv[]) {
+  Solution s;
+  std::cout << s.hasPathSum(general_test_data_prorder(std::vector<int>{
+                                5, 4, 8, 11, -1, 13, 4, 7, 2, -1, -1, -1, 1}),
+                            22)
+            << std::endl;
+  std::cout << s.hasPathSum(
+                   general_test_data_prorder(std::vector<int>{1, 2, 3}), 5)
+            << std::endl;
+  std::cout << s.hasPathSum(general_test_data_prorder(std::vector<int>{1, 2}),
+                            1)
+            << std::endl;
+}
